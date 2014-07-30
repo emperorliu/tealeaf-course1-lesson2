39d2f779dd10869689cbd0c99547c67a3e4a65da
emperorliu <mangomystery@gmail.com>
2014-07-30 16:19:24 -0700

    add the object model exercises

---
+++ the_object_model.rb
@@ -0,0 +1,22 @@
+1. We create an object by initializing it as an instance of a class. For example,
+
+class Human
+end
+
+jeff = Human.new
+
+2. A module is a collection of behaviors useable in other classes via mixins. A module is "mixed in" to a class using the include reserved word.
+
+module Eat
+    def eat(food)
+      puts "I'm eating #{food}."
+    end
+end
+
+class Human
+  include Eat
+end
+
+jeff = Human.new
+jeff.eat("Ramen")
+
